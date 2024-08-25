36c4f8271adf1a0b0d096c9402d4663a48c26144
Dmitrty Myagkov <d.myagkov@forpeople.company>
2024-08-25 16:25:22 +0500

    gRPC client

---
+++ gRPC/CleintGrpcLern/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Net.Client;
+
+namespace CleintGrpcLern;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello,It's the gRpc client!!");
+        using var channel = GrpcChannel.ForAddress("http://localhost:5120");
+
+        while (true)
+        {
+            Console.Write("Введите номер канала: ");
+            var chanellNumber = Console.ReadLine();
+            if (chanellNumber == "1")
+            {
+                var client = new Greeter.GreeterClient(channel);
+                Console.Write("Введите имя: ");
+                string? name = Console.ReadLine();
+                var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+                Console.WriteLine($"Ответ сервера: {reply.Message}");
+            }
+
+            if (chanellNumber == "2")
+            {
+                var client = new LocalRpc.LocalRpcClient(channel);
+                Console.Write("Введите сигнал: ");
+                string signal = Console.ReadLine();
+                Console.Write("Введите описание: ");
+                var desc = Console.ReadLine();
+                var reply = await client.GetLocalSignalAsync(new Request()
+                {
+                    Signal = int.Parse(signal), 
+                    Desc = desc
+                });
+                var result = reply.OutMessage is null ? reply.ErrorId.ToString() : reply.OutMessage;
+                Console.WriteLine($"Ответ сервера: {result}");
+            }   
+        }
+    }
+}
---
+++ gRPC/GrpcLearn/Services/LocalRpcService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+
+namespace GrpcLearn.Services;
+
+public class LocalRpcService : LocalRpc.LocalRpcBase
+{
+    public LocalRpcService()
+    {
+        
+    }
+
+    public override Task<Response> GetLocalSignal(Request request, ServerCallContext context)
+    {
+        try
+        {
+            if (request.Signal == -1)
+                throw new Exception("Exception 1");
+            return Task.FromResult(new Response() { OutMessage = request.GetHashCode().ToString(), ErrorId = 0});
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Task.FromResult(new Response() { OutMessage = null, ErrorId = 500});       
+        }
+        
+    }
+}
