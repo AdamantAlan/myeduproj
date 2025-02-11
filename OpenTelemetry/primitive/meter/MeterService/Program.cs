517ac66305a3c00202ef7a391c79ffab6460a6c5
Myagkov Dmitry <dark.alan1137@gmail.com>
2025-02-11 22:21:58 +0500

    prometheus collect meter

---
+++ OpenTelemetry/primitive/meter/MeterService/Program.cs
@@ -0,0 +1,36 @@
+using OpenTelemetry.Metrics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddAspNetCoreInstrumentation();
+        builder.AddRuntimeInstrumentation();
+        builder.AddHttpClientInstrumentation();
+        builder.AddConsoleExporter();
+        builder.AddPrometheusExporter();
+    });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapPrometheusScrapingEndpoint();
+app.MapControllers();
+
+app.Run();
