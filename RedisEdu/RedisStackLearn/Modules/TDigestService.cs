ad847717e29c92cc4c95392f9660bda55bbc9c32
Myagkov Dmitry <dark.alan1137@gmail.com>
2025-05-29 23:38:47 +0500

    redis modules

---
+++ RedisEdu/RedisStackLearn/Modules/TDigestService.cs
@@ -0,0 +1,26 @@
+﻿using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+
+namespace RedisStackLearn.Modules
+{
+    //Используется для анализа распределения числовых данных — например, латентность, суммы заказов, задержки, температуры и т.п.
+    public class TDigestService
+    {
+        public async Task Example()
+        {
+            var redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+            var db = redis.GetDatabase();
+            var tdigest = db.TDIGEST();
+
+            await tdigest.CreateAsync("latency:metrics", compression: 100);
+
+            await tdigest.AddAsync("latency:metrics", 100, 150, 200, 300, 250, 120, 110);
+
+            // 3. Получение 50-го (медиана), 90-го и 99-го процентилей
+            var percentiles = await tdigest.QuantileAsync("latency:metrics", 0.5, 0.9, 0.99);
+
+            var min = await tdigest.MinAsync("latency:metrics");
+            var max = await tdigest.MaxAsync("latency:metrics");
+        }
+    }
+}
---
+++ RedisEdu/RedisStackLearn/Modules/TimeSerieService.cs
@@ -0,0 +1,60 @@
+﻿using NRedisStack.DataTypes;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+
+namespace RedisStackLearn.Modules
+{
+    //хранения временных меток с числовыми значениями,
+
+    //агрегации (среднее, максимум, сумма и т.д.),
+
+    //уплотнения (downsampling),
+
+    //автоматической retention(удаление старых данных).
+
+    [Obsolete("Старая апишка временных рядов, нужно глянуть новую")]
+    public class TimeSerieService
+    {
+        public async Task Example()
+        {
+            var redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+            var db = redis.GetDatabase();
+            var ts = db.TS();
+
+            // 1. Создание ряда
+            await ts.CreateAsync("sensor:temp", retentionTime: 36000);
+
+            // 2. Добавление точки (время: сейчас, значение: 21.5°C)
+            await ts.AddAsync("sensor:temp", DateTime.UtcNow, 21.5);
+
+            // 3. Получение последнего значения
+            var latest = await ts.GetAsync("sensor:temp");
+            Console.WriteLine($"Время: {latest.Time}, Значение: {latest.Val}");
+
+            // 4. Получение диапазона значений за последние 10 минут
+            var now = DateTime.UtcNow;
+            var data = await ts.RangeAsync("sensor:temp", now.AddMinutes(-10), now);
+            foreach (var point in data)
+                Console.WriteLine($"{point.Time}: {point.Val}");
+
+            await ts.CreateAsync("sensor:temp", retentionTime: 36000, labels: new[] {
+                new TimeSeriesLabel("sensor", "office"),
+                new TimeSeriesLabel("type", "temperature") 
+            });
+
+            var dataRange = await ts.MRangeAsync(
+                now.AddMinutes(-10),
+                now,
+                new[] { "sensor=office" });
+
+            // Создаём исходный ряд
+            await ts.CreateAsync("cpu:raw", retentionTime: 36000);
+
+            // Создаём агрегированный ряд
+            await ts.CreateAsync("cpu:avg:10s", retentionTime: 36000);
+
+            // Создаём правило агрегации: среднее значение за каждые 10 секунд
+            await ts.CreateRuleAsync("cpu:raw", new TimeSeriesRule("cpu:avg:10s", 10, NRedisStack.Literals.Enums.TsAggregation.Avg));
+        }
+    }
+}
