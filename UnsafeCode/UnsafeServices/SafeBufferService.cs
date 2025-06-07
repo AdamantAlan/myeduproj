fcf538f6d7934a2f74a3b66f80a4338c87711e66
Myagkov Dmitry <dark.alan1137@gmail.com>
2025-06-07 23:21:15 +0500

    unsafe safeHandle safeBuffer

---
+++ UnsafeCode/UnsafeServices/SafeBufferService.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnsafeCode.UnsafeServices
+{
+    public sealed class MySafeBuffer : SafeBuffer
+    {
+        public MySafeBuffer() : base(true) { }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        public void Init(IntPtr pointer, uint size)
+        {
+            SetHandle(pointer); // передаём raw-указатель
+            Initialize(size);  // инициализация SafeBuffer
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeHGlobal(handle); // освобождаем память
+            return true;
+        }
+    }
+
+    public class SafeBufferService
+    {
+        public unsafe void Example()
+        {
+            IntPtr nativePtr = Marshal.AllocHGlobal(100);
+            using (var buffer = new MySafeBuffer())
+            {
+                buffer.Init(nativePtr, 100);
+                buffer.Write(4, 123);//int
+                var buf = buffer.Read<int>(0);
+            }
+        }
+    }
+}
---
+++ UnsafeCode/UnsafeServices/SafeHandleService.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace UnsafeCode.UnsafeServices
+{
+    public sealed class MySafeHandle : SafeHandle
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        public MySafeHandle() : base(IntPtr.Zero, true) { }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle()
+        {
+            return CloseHandle(handle);
+        }
+    }
+
+    public class SafeHandleService
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static unsafe extern MySafeHandle CreateFile(sbyte* fileName);
+
+        public unsafe void Example()
+        {
+            string name = "TestName";
+            byte[] nameBytes = Encoding.ASCII.GetBytes(name + '\0');
+            fixed (byte* nameBytesPtr = nameBytes)
+            {
+                using (var handle = CreateFile((sbyte*)nameBytesPtr))
+                {
+                    // работа с файлом
+                } // автоматически вызовется ReleaseHandle
+            }
+        }
+    }
+}
